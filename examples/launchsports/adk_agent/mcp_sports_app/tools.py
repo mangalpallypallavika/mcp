7cedcc67a013dc88d66f0ce3101dd5481935c08a
Pallavika <mangalpallypallavika@gmail.com>
2026-03-30 11:09:45 +0530

    Create tools.py

---
+++ examples/launchsports/adk_agent/mcp_sports_app/tools.py
@@ -0,0 +1,38 @@
+import os
+import dotenv
+import google.auth
+import google.auth.transport.requests
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
+from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnectionParams
+
+MAPS_MCP_URL = "https://mapstools.googleapis.com/mcp"
+BIGQUERY_MCP_URL = "https://bigquery.googleapis.com/mcp"
+
+def get_maps_mcp_toolset():
+    dotenv.load_dotenv()
+    maps_api_key = os.getenv('MAPS_API_KEY', 'no_api_found')
+    tools = MCPToolset(
+        connection_params=StreamableHTTPConnectionParams(
+            url=MAPS_MCP_URL,
+            headers={"X-Goog-Api-Key": maps_api_key}
+        )
+    )
+    print("Maps MCP Toolset configured.")
+    return tools
+
+def get_bigquery_mcp_toolset():
+    credentials, project_id = google.auth.default(
+        scopes=["https://www.googleapis.com/auth/bigquery"]
+    )
+    credentials.refresh(google.auth.transport.requests.Request())
+    tools = MCPToolset(
+        connection_params=StreamableHTTPConnectionParams(
+            url=BIGQUERY_MCP_URL,
+            headers={
+                "Authorization": f"Bearer {credentials.token}",
+                "x-goog-user-project": project_id
+            }
+        )
+    )
+    print("BigQuery MCP Toolset configured.")
+    return tools
